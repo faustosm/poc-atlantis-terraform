3b758fd9f06618c780ba09a07065b96d8f2edfe8
Fausto Soares Machado <faustosoares.m@gmail.com>
2025-11-09 16:52:20 -0300

    fix: ajuste comandos terraform no  atlantis.yaml

--- backend.tf
+++ backend.tf
@@ -1,15 +1,9 @@
 terraform {
   backend "s3" {
-<<<<<<< HEAD
-    bucket = "var.bucket_name"
-    key    = "global/terraform.tfstate"
-    region = "var.aws_region"
-=======
-    bucket = "poc-atlantis-terraform"
-    key    = "global/terraform.tfstate"
-    region = "sa-east-1"
->>>>>>> infracost-test
+    bucket         = "poc-atlantis-terraform"
+    key            = "global/terraform.tfstate"
+    region         = "sa-east-1"
     dynamodb_table = "terraform-locks"
-    encrypt = true
+    encrypt        = true
   }
 }
